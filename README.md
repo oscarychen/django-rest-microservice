26dadaa10c0804210b3479ef9e595be2140a0ded
Oscar Chen <oscar.y.chen@gmail.com>
2022-02-05 09:58:11 -0700

    set up project template

---
+++ README.md
@@ -0,0 +1,3 @@
+# django-rest-microservice
+
+This is a place holder for work currently in progress and not yet published.
---
+++ rest_framework_microservice/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RestFrameworkMicroserviceConfig(AppConfig):
+    name = 'rest_framework_microservice'
